616517defd59f67087e6ec2be2e437e4c4d21a1e
sunil <sunil_kmr19@yahoo.co.in>
2018-07-08 02:10:00 +0530

    fixed rout root issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.11.0"
 set :application, 'contactbook'
 set :repo_url, 'git@github.com:sunil123456/contactbook.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/ubuntu/contactbook'
+set :deploy_to, '/home/contactbook'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
